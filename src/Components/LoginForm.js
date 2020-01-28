a8084134b13765e326e9cf6c8172684e98011a5b
Lee-h-saem <53569830+Lee-h-saem@users.noreply.github.com>
2020-01-28 11:33:12 +0900

    styled-components, hook form 추가

--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -1,44 +1,82 @@
-import React, { useState } from "react";
-import "./LoginForm.css";
+import React from "react";
+import styled from "styled-components";
+import { useForm } from "react-hook-form";
 
 export function LoginForm() {
-  const [email, setEmail] = useState();
-  const [pass, setPass] = useState();
+  const { register, handleSubmit, errors } = useForm();
 
-  const handleSubmit = e => {
-    e.preventDefault();
-    if ((email, pass) === undefined) {
-      alert("이메일과 패스워드를 입력하세요.");
-    } else;
-    alert(`E-mail: ${email},Password: ${pass}`);
+  const onSubmit = data => {
+    console.log(data);
   };
 
   return (
-    <div>
-      <div className="login-container">
-        <form type="submit" onSubmit={handleSubmit}>
-          <label>
-            E-mail{" "}
-            <input
-              type="text"
-              placeholder="E-mail"
-              value={email}
-              onChange={e => setEmail(e.target.value)}
-            />
-          </label>
-          <label>
-            Password{" "}
-            <input
-              type="password"
-              name="password"
-              placeholder="password"
-              value={pass}
-              onChange={e => setPass(e.target.value)}
-            />
-          </label>
-          <button>Log in</button>
+    <Styles>
+      <section>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input
+            type="text"
+            name="Email"
+            placeholder="E-mail"
+            ref={register({ required: true, pattern: /^\S+@\S+$/i })}
+          />
+          {errors.Email && "이메일 양식이 일치하지 않습니다."}
+          <input type="password" name="pass" placeholder="P.W" ref={register} />
+          <button type="submit">Log In</button>
         </form>
-      </div>
-    </div>
+      </section>
+    </Styles>
   );
 }
+const Styles = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  & section {
+    margin: 0;
+    padding: 10px;
+    width: 500px;
+    height: 550px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    & form {
+      margin: 0;
+      padding: 15px;
+      width: 300px;
+      height: 350px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      background: #b8b6ffa3;
+      border-radius: 10px;
+      & input {
+        margin-top: 20px;
+        margin-bottom: 20px;
+        width: 250px;
+        height: 30px;
+        padding-left: 10px;
+        letter-spacing: 2px;
+        font-size: 1rem;
+        border: none;
+        border-bottom: 1px solid lightseagreen;
+        background: none;
+        outline: none;
+      }
+      & button {
+        width: 140px;
+        height: 30px;
+        border-radius: 5px;
+        background: lightgreen;
+        font-size: 1rem;
+        :hover {
+          background: #72c672;
+          color: blanchedalmond;
+        }
+      }
+    }
+  }
+`;
